f1cc5c3cc4a6f3caecfd95739e29deeb139c3333
tuananhpta19 <tuananh.pta19@gmail.com>
2021-07-21 01:21:34 +0700

    add list product

--- client/src/api/product.js
+++ client/src/api/product.js
@@ -1,3 +1,4 @@
 import { get, post } from '../utils/axios';
 
 export const createProductAPI = (body) => post("/api/product",body)
+export const getAllProductAPI = () => get("/api/product")
--- client/src/views/Admin/Product/ListProduct.js
+++ client/src/views/Admin/Product/ListProduct.js
@@ -1,126 +1,92 @@
-// import React, { useEffect, useState } from 'react';
-// import { Table, Tag, Popconfirm, Button } from 'antd';
-// // import CreateUserModal from './CreateUser';
-// import BaseLayoutAdmin from '../../../components/BaseLayoutAdmin/BaseLayoutAdmin';
-// import { getAllUserAPI, updateStatusAPI } from '../../../api/user';
-// import { notificationError, notificationSuccess } from '../../../utils/notification';
-// import {
-//     Link
-// } from 'react-router-dom';
-// let DashboardUser = (props) => {
-//     let [dataListUser, setDataListUser] = useState([]);
-//     let [visibleModalCreateUser, setVisibleModalCreateUser] = useState(false);
-//     let [isCreateUserSuccess, setIsCreateUserSuccess] = useState(false);
-//     let [isChangeStatusSuccess, setIsChangeStatusSuccess] = useState(false);
-//     useEffect(() => {
-//         let _getListUsers = async () => {
-//             try {
-//                 let res = await getAllUserAPI();
-//                 if (res.status === 200) {
-//                     let handleData = res.data.data ? res.data.data.map((userItem, index) => {
-//                         return {
-//                             key: index + 1,
-//                             ...userItem
-//                         }
-//                     }) : [];
-//                     setDataListUser(handleData)
-//                 }
-//             } catch (error) {
-//                 notificationError(error)
-//             }
-//         }
-//         _getListUsers()
-//     }, [isCreateUserSuccess, isChangeStatusSuccess])
+import React, { useEffect, useState } from 'react';
+import { Table, Tag, Popconfirm, Button } from 'antd';
+import BaseLayoutAdmin from '../../../components/BaseLayoutAdmin/BaseLayoutAdmin';
+import { getAllProductAPI } from '../../../api/product';
+import { notificationError, notificationSuccess } from '../../../utils/notification';
+import {
+    Link
+} from 'react-router-dom';
+let DashboardUser = (props) => {
+    let [dataListProduct, setDataListProduct] = useState([]);
+    useEffect(() => {
+        let _getListProducts = async () => {
+            try {
+                let res = await getAllProductAPI();
+                if (res.status === 200) {
+                    let handleData = res.data.data ? res.data.data.map((productItem, index) => {
+                        return {
+                            key: index + 1,
+                            ...productItem
+                        }
+                    }) : [];
+                    setDataListProduct(handleData)
+                }
+            } catch (error) {
+                notificationError(error)
+            }
+        }
+        _getListProducts()
+    }, [])
 
-//     let handleBlockUser = async (record, status) => {
-//         try {
-//             let res = await updateStatusAPI(record._id, { status: status });
-//             if (res.status === 200) {
-//                 setIsChangeStatusSuccess(!isChangeStatusSuccess)
-//                 notificationSuccess("Cập nhật trạng thái thành công")
-//             }
-//         } catch (error) {
-//             notificationError(error)
-//         }
-//     }
-//     const columns = [
-//         {
-//             title: 'key',
-//             dataIndex: 'key',
-//             key: 'key',
-//         },
-//         {
-//             title: 'Tên',
-//             dataIndex: 'name',
-//             key: 'name',
-//         },
-//         {
-//             title: 'email',
-//             dataIndex: 'email',
-//             key: 'email',
-//         },
-//         {
-//             title: 'trạng thái',
-//             dataIndex: 'status',
-//             key: 'status',
-//             render: (status) => {
-//                 let checkStatus = (status) => {
-//                     if (status === "active") {
-//                         return <Tag color="green">active</Tag>
-//                     }
-//                     return <Tag color="red">block</Tag>
-//                 }
-//                 return (
-//                     <>
-//                         {checkStatus(status)}
-//                     </>
-//                 )
-//             }
-//         },
-//         {
-//             title: 'Hành động',
-//             render: (record) => (
-//                 <>
-//                     <Popconfirm
-//                         title="Bạn có chắc chắn muốn thay đổi"
-//                         onConfirm={() => {
-//                             if (record.status === "active") {
-//                                 handleBlockUser(record, "block")
-//                             } else {
-//                                 handleBlockUser(record, "active")
-//                             }
-
-//                         }}
-//                         okText="Đồng ý"
-//                         cancelText="Không"
-//                     >
-//                         {record.status === "active" ? <Button style={{ background: 'red', color: 'white' }}>Block</Button> : <Button>Active</Button>}
-//                     </Popconfirm>
-//                     <Button style={{ background: '#8bc34a', color: 'black', marginLeft: "30px" }}> 
-//                         <Link to={"/user/"+record._id}>Xem chi tiết</Link>
-//                     </Button>
-//                 </>
+    const columns = [
+        {
+            title: 'key',
+            dataIndex: 'key',
+            key: 'key',
+        },
+        {
+            title: 'Tên',
+            dataIndex: 'name',
+            key: 'name',
+        },
+        {
+            title: 'Giá gốc',
+            dataIndex: 'price',
+            key: 'price',
+        },
+        {
+            title: 'Giá bán',
+            dataIndex: 'sell',
+            key: 'sell',
+        },
+        {
+            title: 'Số lượng',
+            dataIndex: 'quantity',
+            key: 'quantity',
+        },
+        {
+            title: 'Hành động',
+            render: (record) => (
+                <>
                 
-//             ),
-//         },
-//     ];
+                    <Button style={{ background: '#8bc34a', color: 'black', marginLeft: "30px" }}> 
+                        <Link to={"/user/"+record._id}>Xem chi tiết</Link>
+                    </Button>
+                    <Popconfirm
+                        title="Bạn có chắc chắn muốn xóa sản phẩm"
+                        onConfirm={() => {
+                            // if (record.status === "active") {
+                            //     handleBlockUser(record, "block")
+                            // } else {
+                            //     handleBlockUser(record, "active")
+                            // }
 
-//     return (
-//         <BaseLayoutAdmin>
-//             {/* <CreateUserModal
-//                 visibleModalCreateUser={visibleModalCreateUser}
-//                 setVisibleModalCreateUser={setVisibleModalCreateUser}
-//                 setIsCreateUserSuccess={setIsCreateUserSuccess}
-//                 isCreateUserSuccess={isCreateUserSuccess}
-//             ></CreateUserModal> */}
-//             <div className="user-action" style={{ textAlign: 'right', margin: '30px 0', width: '100%' }}>
-//                 <Button type='primary' onClick={() => { setVisibleModalCreateUser(true) }}>
-//                    danh sách sản phảm
-//                 </Button>
-//             </div>
+                        }}
+                        okText="Đồng ý"
+                        cancelText="Không"
+                    >
+                      <Button style={{ background: 'red', color: 'white',marginLeft: "30px" }}>Xóa</Button>
+                    </Popconfirm>
+                </>
+                
+            ),
+        },
+    ];
 
-//             <Table columns={columns} dataSource={dataListUser}></Table>
-//         </BaseLayoutAdmin>
-//     );
-// }
-// export default DashboardUser
+    return (
+        <BaseLayoutAdmin>
+            <Table columns={columns} dataSource={dataListProduct}></Table>
+        </BaseLayoutAdmin>
+    );
+}
+export default DashboardUser
